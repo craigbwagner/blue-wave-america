4ffe8e0fe1a5758acab4a09f6564fe8898249c4b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-25 17:56:08 -0400

    add swiper pagination, navigation, fade effect, autoplay modules

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,25 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 
 export default function Carousel() {
   return (
     <Swiper
-      modules={[Autoplay]}
+      effect={"fade"}
+      autoplay={{
+        delay: 4000,
+        pauseOnMouseEnter: true
+      }}
+      pagination={{
+        clickable: true
+      }}
+      navigation={true}
       spaceBetween={50}
       slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+      modules={[Autoplay, Pagination, Navigation, EffectFade]}
     >
       <SwiperSlide>
         <img src='/graphics/stock1.jpg' />
