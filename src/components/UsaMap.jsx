e828b9c94dfc294f04b54df9caf16cb11164ccaa
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-17 18:41:31 -0400

    add react-tooltips and display tooltips when hovering over individual states

--- src/components/UsaMap.jsx
+++ src/components/UsaMap.jsx
@@ -1,4 +1,5 @@
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { Tooltip } from "react-tooltip";
 
 const geoUrl =
   "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
@@ -7,46 +8,68 @@ const highlightedStates = {
   Virginia: "#0285c7"
 }
 
+const allStates = [
+  {name: "Virginia",},
+]
+
 export default function UsaMap() {
+
   return (
-    <ComposableMap projection="geoAlbersUsa" className="max-w-4xl mx-auto">
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => {
-            const stateName = geo.properties.name;
-            let stateUrl = "#";
-            const fillColor = highlightedStates[stateName] || "#D6D6DA";
+    <>
+      <ComposableMap projection="geoAlbersUsa" className="max-w-4xl mx-auto">
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const stateName = geo.properties.name;
+              let stateUrl = "#";
+              const fillColor = highlightedStates[stateName] || "#D6D6DA";
 
-            if (stateName in highlightedStates) {
-              stateUrl = `/states/#${stateName}`;
-            }
+              if (stateName in highlightedStates) {
+                stateUrl = `/states/#${stateName}`;
+              }
 
-            return (
-              <a href={stateUrl}>
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={() => alert(`Clicked on ${stateName}`)}
-                  style={{
-                    default: {
-                      fill: fillColor,
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#87878a",
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#87878a",
-                      outline: "none",
-                    },
-                  }}
-                />
-              </a>
-            );
-          })
-        }
-      </Geographies>
-    </ComposableMap>
+              return (
+                <a href={stateUrl}
+                  id={stateName}>
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    onClick={() => alert(`Clicked on ${stateName}`)}
+                    style={{
+                      default: {
+                        fill: fillColor,
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: fillColor,
+                        outline: "none",
+                      },
+                      pressed: {
+                        fill: fillColor,
+                        outline: "none",
+                      },
+                    }}
+                  />
+                </a>
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+
+      {allStates.map((state) => (
+        <Tooltip
+          anchorSelect={`#${state.name}`}
+          id={state.name}
+          key={state.name}
+          place="top"
+          className="bg-blue-500 text-white p-2 rounded"
+        >
+          <div className="flex flex-col text-center">
+            <span>{state.name}</span>
+          </div>
+        </Tooltip>
+      ))}
+    </>
   );
 }
